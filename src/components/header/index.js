d369d87b4b4dad374972b1a22d0ebabafa4227dc
birne9 <17317296268@163.com>
2023-02-02 13:38:32 +0800

    页面头部组件

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import "./index.css";
+const Header = () => {
+  return (
+    <>
+      <div class="header">
+        <div class='left'>Birne9</div>
+        <div class='right'>
+          <div>About</div>
+          <div>Blog</div>
+          <div>Github</div>
+          <div class='darkroom'>Dark room</div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Header;
